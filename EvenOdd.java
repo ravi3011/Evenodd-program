53de20b0aace6276825a633f46faef508ec665ff
ravi <ravikushwaharavi198@gmail.com>
2018-12-21 10:35:10 +0530

    adding even odd program

---
+++ EvenOdd.java
@@ -0,0 +1,15 @@
+class EvenOdd
+{
+	public static void main(String args[])
+	{
+		if(10%2==0)
+		{
+		System.out.println("number is even");	
+		}
+		else
+		{
+			System.out.println("number is odd");
+		}
+		
+	}
+} 
